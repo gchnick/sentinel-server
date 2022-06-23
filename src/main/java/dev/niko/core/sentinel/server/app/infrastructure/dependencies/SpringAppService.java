9f0db155d1dfb0f52fbea7fa06b2e1d23c72701f
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-06-23 16:37:54 +0000

    Fix SpringAppService dependencies

--- src/main/java/dev/niko/core/sentinel/server/app/infrastructure/dependencies/SpringAppService.java
+++ src/main/java/dev/niko/core/sentinel/server/app/infrastructure/dependencies/SpringAppService.java
@@ -4,22 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import dev.niko.core.sentinel.server.app.domain.AppRepo;
-import dev.niko.core.sentinel.server.app.domain.AppService;
 import dev.niko.core.sentinel.server.app.domain.AppServiceImp;
 import dev.niko.core.sentinel.server.app.domain.update.UpdateRepo;
-import lombok.RequiredArgsConstructor;
 
 @Configuration
-@RequiredArgsConstructor
 public class SpringAppService {
 
-    private final AppRepo appRepo;
-    private final UpdateRepo updateRepo;
-
-
     @Bean
-    public AppService springAppService() {
+    public AppServiceImp appService(AppRepo appRepo, UpdateRepo updateRepo) {
         return new AppServiceImp(appRepo, updateRepo);
-    }
-    
+    }    
 }
