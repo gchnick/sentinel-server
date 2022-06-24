ec21924e13a3f749ffc22c94f4719c484de82e88
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-06-24 03:26:40 +0000

    Fix bug Null Pointer Exception in App

--- src/main/java/dev/niko/core/sentinel/server/app/domain/App.java
+++ src/main/java/dev/niko/core/sentinel/server/app/domain/App.java
@@ -72,6 +72,10 @@ public class App extends Entity implements AggregateRoot {
             throw new VersionUpdateIsLessException(VERSION_UPDATE_IS_LESS);
         }
 
+        if(updates == null) {
+            updates = new ArrayList<>();
+        }
+
         updates.add(update);
         currentVersion = update.getVersion();
     }
