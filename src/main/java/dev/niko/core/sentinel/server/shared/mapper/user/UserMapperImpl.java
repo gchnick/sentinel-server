41a59482509d6bef8a047058d7a5467675387f73
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-06-25 18:16:39 +0000

    Registre UserMapper in context spring

--- src/main/java/dev/niko/core/sentinel/server/shared/mapper/user/UserMapperImpl.java
+++ src/main/java/dev/niko/core/sentinel/server/shared/mapper/user/UserMapperImpl.java
@@ -2,12 +2,15 @@ package dev.niko.core.sentinel.server.shared.mapper.user;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import dev.niko.core.sentinel.server.application.user.request.UserRequest;
 import dev.niko.core.sentinel.server.domain.security.UserRole;
 import dev.niko.core.sentinel.server.infrastructure.user.mapping.RoleMap;
 import dev.niko.core.sentinel.server.infrastructure.user.mapping.UserMap;
 import dev.niko.core.sentinel.server.shared.mapper.DataMapperException;
 
+@Component
 public class UserMapperImpl implements UserMapper {
 
     @Override
