525eefe5f6893c2be5f1501521296a5a6c97ff5e
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-06-25 18:59:12 +0000

    Remove sql sentences and add command line runner

--- src/main/java/dev/niko/core/sentinel/server/ServerApplication.java
+++ src/main/java/dev/niko/core/sentinel/server/ServerApplication.java
@@ -1,7 +1,16 @@
 package dev.niko.core.sentinel.server;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import dev.niko.core.sentinel.server.application.user.request.UserRequest;
+import dev.niko.core.sentinel.server.infrastructure.user.service.UserService;
+import dev.niko.core.sentinel.server.shared.mapper.user.UserMapper;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +19,15 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(UserService service, UserMapper mapper){
+		return args -> {
+			List<String> admin = new ArrayList<>();
+			admin.add("ROLE_ADMIN");
+			
+			service.create(
+				mapper.toMap(new UserRequest("rootadmin", "rootpass", admin))
+			);
+		};
+	}
 }
