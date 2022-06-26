1dde19330128ac845621727f20dd8fbd45addbef
Nick Galán <72513179+gchnick@users.noreply.github.com>
2022-06-26 03:34:26 +0000

    Add properties to environments and add download update resource

--- src/main/java/dev/niko/core/sentinel/server/application/app/AppResource.java
+++ src/main/java/dev/niko/core/sentinel/server/application/app/AppResource.java
@@ -3,16 +3,26 @@ package dev.niko.core.sentinel.server.application.app;
 import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.HttpStatus.CREATED;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static java.nio.file.Files.copy;
 
-import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,7 +40,6 @@ import dev.niko.core.sentinel.server.application.shared.Response;
 import dev.niko.core.sentinel.server.domain.App;
 import dev.niko.core.sentinel.server.domain.AppService;
 import dev.niko.core.sentinel.server.domain.Update;
-import dev.niko.core.sentinel.server.domain.exception.ConflictException;
 import dev.niko.core.sentinel.server.shared.mapper.AppMapper;
 import dev.niko.core.sentinel.server.shared.mapper.update.UpdateMapper;
 import lombok.RequiredArgsConstructor;
@@ -40,17 +49,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AppResource {
 
-    private static final String ERROR_UPLOADS = "Internal server error when uploading file. ";
+    private static final String FILE_NOT_FOUNT = "File not found.";
     private static final String ROLE_ADMIN = "ROLE_ADMIN";
     private static final String ROLE_DEV = "ROLE_DEV";
     private static final String ROLE_CLIENT = "ROLE_CLIENT";
+    private static final String PATH = System.getProperty("user.home") + "/sentinel/uploads/";
     
     private final AppService appService;
     private final AppMapper appMapper;
     private final UpdateMapper updateMapper;
-
-    @Value("${config.uploads.path}")
-    private String path;
 
     @Secured({ROLE_ADMIN, ROLE_DEV})
     @PostMapping
@@ -82,6 +89,33 @@ public class AppResource {
         );
     }
 
+    /**
+     * TODO Usar excepciones conocidas
+     * TODO implementar actualizacion en un solo paso
+     */
+    @Secured(ROLE_CLIENT)
+    @GetMapping("/{uid}/{version}/update")
+    public ResponseEntity<?> update(@PathVariable UUID uid, @PathVariable String version) throws IOException {
+
+        App app = appService.get(uid);
+        String file = app.getUpdateURL();
+        Path filePath = Paths.get(PATH, file).toAbsolutePath().normalize();
+
+        if(!Files.exists(filePath)) {
+            throw new FileNotFoundException(FILE_NOT_FOUNT);
+        }
+
+        Resource resource = new UrlResource(filePath.toUri());
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("File-Name", file);
+        httpHeaders.add("CONTENT_DISPOSITTION", "attachment;File-Name=" + resource.getFilename());
+
+        return ResponseEntity.ok()
+            .contentType(MediaType.parseMediaType(Files.probeContentType(filePath)))
+            .headers(httpHeaders)
+            .body(resource);
+    }
+
     @Secured(ROLE_DEV)
     @PostMapping("/{uid}/name")
     public ResponseEntity<Response> edit(@PathVariable UUID uid, @RequestBody @Valid AppRequest app) {
@@ -98,34 +132,19 @@ public class AppResource {
 
     @Secured({ROLE_ADMIN, ROLE_DEV})
     @PostMapping("/{uid}/release")
-    public ResponseEntity<Response> releaseUpdate(@PathVariable UUID uid, @Valid UpdateRequest update, @RequestParam(required = true) MultipartFile file) {
+    public ResponseEntity<Response> releaseUpdate(@PathVariable UUID uid, @Valid UpdateRequest update, @RequestParam(required = true) MultipartFile file) throws IOException {
+
         App app = appService.get(uid);
-        String appName = fixString(app.getName());
-        String filename = randomFilename(file.getOriginalFilename());
-        String updateURL = new StringBuilder()
-            .append(appName)
-            .append("/")
-            .append(filename)
-            .toString();
-
-        String folderPath = new StringBuilder()
-            .append(path)
-            .append(appName)
-            .append("/")
-            .toString();
-
-        String filePath = folderPath.concat(filename);
+        String appName = StringUtils.cleanPath(app.getName());
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+        String randomFilename = randomFilename(filename);
+        Path fileStorage = Paths.get(PATH, appName, randomFilename).toAbsolutePath().normalize();
+        Path url = Paths.get(appName, randomFilename).toAbsolutePath().normalize();
         
-        try {
-            new File(folderPath).mkdir();
-            file.transferTo(new File(filePath));
-            app.setUpdateURL(updateURL);
-        } catch (Exception e) {
-            throw new ConflictException(ERROR_UPLOADS.concat(e.getMessage()));
-        }
+        copy(file.getInputStream(), fileStorage, REPLACE_EXISTING);  
+        app.setUpdateURL(url.toString());
 
         Update request = updateMapper.toDomain(update);
-        
         appService.releaseUpdate(app, request);
         return ResponseEntity.ok(
             Response.builder()
@@ -148,12 +167,6 @@ public class AppResource {
         return UUID.randomUUID()
             .toString()
             .concat("-")
-            .concat(fixString(filename));
-    }
-
-    private String fixString(String str) {
-        return str.replace(" ", "")
-            .replace(":", "")
-            .replace("\\", "");
+            .concat(filename);
     }
 }
